14c2f42c1dfbb800e82e861794a7ff5c1bb45626
Bharath Chanthala <chanthalabharath2k@gmail.com>
2026-08-08 08:34:28 +0200

    Complete Express CRUD with React frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 
 
 const app = express();
+app.use(express.json());
 
 app.use(cors());
 
@@ -30,6 +31,50 @@ app.get("/students", (req, res) => {
 
     res.json(students);
 
+});
+app.post("/students",(req,res) =>{
+
+    const {name,age}=req.body;
+    const newStudent = {
+        id: students.length +1,
+        name,
+        age 
+    }
+    students.push(newStudent);
+    res.json(newStudent);
+});
+
+app.put("/students/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const {name,age }= req.body;
+    const student = students.find( (student)=>student.id ===id);
+    if (!student){
+        return res.status(404).json({
+            message :"stundent not found"
+        });
+
+    }
+    student.name = name;
+    student.age =age;
+    console.log(student);
+
+    res.json(student);
+
+});
+app.delete("/students/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    const index = students.findIndex((student) => student.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({
+            message: "Student not found"
+        });
+    }
+
+    const deletedStudent = students.splice(index, 1);
+
+    res.json(deletedStudent[0]);
 });
 
 app.listen(3000, () => {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,15 @@ type Student = {
 };
 
 function App() {
+
+  
+const [name , setName] = useState("");
+const [age , setAge] = useState("");
+const [editId, setEditId] = useState<number | null>(null);
+const [editName, setEditName] = useState("");
+const [editAge, setEditAge] = useState("");
+
+
   const [students, setStudents] = useState<Student[]>([]);
 
 
@@ -19,6 +28,57 @@ function App() {
     setStudents(data);
 
 }
+async function addStudent(){
+   const response = await fetch ("http://localhost:3000/students",{
+    method:"POST",
+    headers:{
+      "content-type" :"application/json"
+    },
+    body:JSON.stringify({
+      name,
+      age:Number(age)
+    })
+   });
+  console.log(response.status);
+
+console.log(response.ok);
+
+console.log(response.headers);
+const message = await response.text();
+
+console.log(message);
+  console.log(name);
+  console.log(age);
+  await loadStudents();
+  setName("");
+  setAge("");
+
+}
+async function updateStudent() {
+    if (editId === null) return;
+
+    await fetch(`http://localhost:3000/students/${editId}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: editName,
+            age: Number(editAge)
+        })
+    });
+
+    await loadStudents();
+
+    setEditId(null);
+}
+async function deleteStudent(id: number) {
+    await fetch(`http://localhost:3000/students/${id}`, {
+        method: "DELETE"
+    });
+
+    await loadStudents();
+}
 
   return (
     <div style={{ padding: 20 }}>
@@ -27,14 +87,52 @@ function App() {
       <button onClick={loadStudents}>
         Load Students
       </button>
+      <br/>
+      <br/>
+      <br/><br/>  
+      
+
+      <input type="text" placeholder="name" value={name} onChange={(e)=>setName(e.target.value)} /><br/>
+      <input type="number" placeholder="age" value={age} onChange={(e)=> setAge(e.target.value)} /><br/><br/>
+      <button onClick={addStudent}>Add Student</button>
 
       <hr />
 
       {students.map((student) => (
         <div key={student.id}>
           {student.id}. {student.name} ({student.age})
+          <button
+            onClick={() => {
+                setEditId(student.id);
+                setEditName(student.name);
+                setEditAge(String(student.age));
+            }}
+        >
+            Edit
+        </button>
+        <button onClick={() => deleteStudent(student.id)}>
+    Delete
+</button>
         </div>
       ))}
+      {editId !== null && (
+    <div>
+        <input
+            value={editName}
+            onChange={(e) => setEditName(e.target.value)}
+        />
+
+        <input
+            type="number"
+            value={editAge}
+            onChange={(e) => setEditAge(e.target.value)}
+        />
+
+        <button onClick={updateStudent}>
+            Update
+        </button>
+    </div>
+)}
     </div>
   );
 }
